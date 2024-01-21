8a862f2f3bd348c3cbac8a8f2fe362fd793a89aa
henhadayo1 <henhadayo@gmail.com>
2024-01-21 17:02:46 +0200

    Use cors and allows requests from all origins

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,35 @@ import express, { Request, Response, NextFunction } from "express";
 import { json } from "body-parser";
 import { connect } from "mongoose";
 import "dotenv/config";
+import cors, { CorsOptions } from "cors";
 import todosRouter from "./routes/todo";
+
+// const whiteList = ["http://localhost:5173"];
+
+// const corsOptions: CorsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("origin::", origin);
+//     if (whiteList.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       console.log(`Origin: ${origin} is not allowed`);
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
+
+// Allow requests from all origins
+const corsOptions: CorsOptions = {
+  origin: "*",
+};
 
 run().catch((err: Error) => {
   console.log(err);
 });
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(json());
 
